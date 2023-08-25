34c38bfa6c2f4f8f574f0ff2c4315a8467dd0a49
Alexandre Hastings <alex.hastings.1227@gmail.com>
2023-08-24 23:13:33 -0400

    finish order.js+order-test.js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,36 @@
+function takeOrder(order, orderList){
+  if (orderList.length<3){
+    orderList.push(order)
+  }
+}
+
+function refundOrder(orderNumber, orderList) {
+  for (let i = 0; i < orderList.length; i++) {
+    if (orderList[i].orderNumber === orderNumber) {
+      orderList.splice([i], 1);
+    }
+  }
+}
+
+function listItems(orderList){
+  var items = []
+  for (var i=0;i<orderList.length;i++){
+  items.push(orderList[i].item)
+  }
+  return items.join(", ")
+}
+
+function searchOrder(orderList,itemToSearch){
+  for (var i=0;i<orderList.length;i++){
+      if (orderList[i].item===itemToSearch){
+       return true
+      }
+  } return false
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
