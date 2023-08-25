fba848336317b370c8bcf26a93e04bc80bc30ccf
Alexandre Hastings <alex.hastings.1227@gmail.com>
2023-08-24 21:26:33 -0400

    finish meal+meal-test

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,43 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`;
 }
 
+function createMenuItem(name, price, type){
+  return{
+    name: name,
+    price: price,
+    type: type
+  }
+}
+function addIngredients(ingredients, array){
+  if (!array.includes(ingredients)){
+    return array.push(ingredients)
+  }
+}
+
+function formatPrice(initialPrice){
+  return `$${initialPrice}`
+}
+
+function decreasePrice(price){
+  return price*.9
+}
+
+function createRecipe(title, ingredients, type){
+  return{
+    title: title,
+    ingredients: ingredients,
+    type: type
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
